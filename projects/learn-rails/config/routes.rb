75efea5fe879326f6cfd7f4c6c2c7471c9590185
cbutler <con_butler@hotmail.com>
2016-01-20 19:17:43 +0000

    dynamic home page

--- projects/learn-rails/config/routes.rb
+++ projects/learn-rails/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'visitors#new'
+  #We are routing to the visitors controller here
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
